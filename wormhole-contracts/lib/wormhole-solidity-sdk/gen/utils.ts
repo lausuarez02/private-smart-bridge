828b4a60b172e2d31d189871d3ae4238583bd343
Tomas Oliver <tomi204@outlook.com>
2025-11-20 04:17:59 -0300

    fix: delete old contracts

--- wormhole-contracts/lib/wormhole-solidity-sdk/gen/utils.ts
+++
@@ -1,8 +0,0 @@
-export function toCapsSnakeCase(identifier: string) {
-  return identifier.split('').map((char, index) =>
-    (  char === char.toUpperCase() //insert underscore before uppercase letters
-    && char !== char.toLowerCase() //don't insert underscore before numbers
-    && index !== 0                 //don't insert underscore at the beginning
-    ) ? '_' + char : char.toUpperCase()
-  ).join('');
-}
